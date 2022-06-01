03448d3a4ec928b698d780e09802f97d435288e1
Helenajaegle <94958481+Helenajaegle@users.noreply.github.com>
2022-06-01 12:22:14 +0200

    page 404

---
+++ src/views/404View.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex items-center m-5 text-white">
+      <RouterLink to="/accueil"><p class="text-2xl uppercase text-center m-5 hover:font-bold">Accueil</p></RouterLink>
+      <p>|</p>
+      <RouterLink to="/programmation"><p class="text-2xl uppercase text-center m-5 hover:font-bold">Programmation</p></RouterLink>
+      <p>|</p>
+      <RouterLink to="/concerts"><p class="text-2xl uppercase text-center m-5 hover:font-bold">Concerts</p></RouterLink>
+      <p>|</p>
+      <RouterLink to="/festival"><p class="text-2xl uppercase text-center m-5 hover:font-bold">Festival</p></RouterLink>
+    </div>
+
+    <div class="h-96 mb-5">
+    <img class="w-full m-0 opacity-25" src="../assets/Medias/festival_5.png" alt="Fond concert">
+  </div> 
+  
+    <div class="text-white text-6xl text-center z-20">
+        <h1>Désolé, cette page n'existe pas.</h1>
+    </div>
+
+    <div class="m-10 flex justify-center items-center p-10">
+        <input type="search" class="rounded-lg p-3" placeholder="Recherchez...">
+    </div>
+
+     <div class="flex items-center justify-center mt-60 mb-32">
+        <div class="rounded-full w-2/3 h-2 bg-white"></div>
+    </div>
+
+     <footer>
+    <div class="text-white ml-10 text-2xl">
+      <RouterLink to="/mentions"><p>Mentions légales</p></RouterLink>
+      <RouterLink to="/contact"><p>Contact</p></RouterLink>
+      <RouterLink to="/gds"><p>Guide de style</p></RouterLink>
+    </div>
+
+    <div class="flex float-right mr-12">
+      <img src="../assets/Medias/logo_petit.png" alt="Logo petit">
+    </div>
+
+    <div class="flex float-left space-x-9 m-10">
+      <img src="../assets/Medias/Youtube.png" alt="Youtube">
+      <img src="../assets/Medias/twitter.png" alt="Twitter">
+      <img src="../assets/Medias/instagram.png" alt="Instagram">
+    </div>
+  </footer>
+</template>
